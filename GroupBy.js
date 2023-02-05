61ac5190f64b8022efe933d87e54e5803a94605b
Anthony Okonta <okontaa@gmail.com>
2023-02-05 09:55:27 +0100

    Update GroupBy.js

--- GroupBy.js
+++ GroupBy.js
@@ -1,4 +1,3 @@
-
 function groupBy(arr){
 
 return arr.reduce((names,value)=>{
@@ -11,8 +10,10 @@ names[value]++
 else{
 names[value]=1
 }
-return names,{})
+return names;
+},{})
 
 }
+                  
                   
 console.log(groupBy([1,2,1,23,4,3,2,4,56,3,3,4,2,4,5,6,3,2,3,4,5,4]))
